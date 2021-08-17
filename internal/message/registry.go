a0220349a52283c754116496bc2a88b938004c64
eloylp <eloylpdev@gmail.com>
2021-08-18 00:21:59 +0200

    Add independent registry for messages

--- internal/message/registry.go
+++ internal/message/registry.go
@@ -2,6 +2,8 @@ package message
 
 import (
 	"fmt"
+
+	"google.golang.org/protobuf/proto"
 )
 
 type Registry map[string][]interface{}
@@ -22,4 +24,18 @@ func (r Registry) Handler(key string) ([]interface{}, error) {
 		return nil, fmt.Errorf("cannot found handler with key: %s", key)
 	}
 	return h, nil
+}
+
+type MessageRegistry map[string]proto.Message
+
+func (r MessageRegistry) Register(key string, msg proto.Message) {
+	r[key] = msg
+}
+
+func (r MessageRegistry) Message(key string) (proto.Message, error) {
+	msg, ok := r[key]
+	if !ok {
+		return nil, fmt.Errorf("cannot found message with key: %s", key)
+	}
+	return msg, nil
 }
--- internal/message/registry_test.go
+++ internal/message/registry_test.go
@@ -8,6 +8,7 @@ import (
 	"github.com/stretchr/testify/require"
 
 	"go.eloylp.dev/goomerang/internal/message"
+	"go.eloylp.dev/goomerang/internal/message/test"
 )
 
 type fakeHandler func() error
@@ -46,4 +47,19 @@ func TestMultipleCumulativeHandlersCanBeRegistered(t *testing.T) {
 	handlers, err := r.Handler("m")
 	require.NoError(t, err)
 	assert.Len(t, handlers, 4)
+}
+
+func TestMessageRegistry(t *testing.T) {
+	msg := &test.GreetV1{Message: "hi!"}
+	r := message.MessageRegistry{}
+	r.Register("m1", msg)
+	res, err := r.Message("m1")
+	require.NoError(t, err)
+	require.Equal(t, msg, res)
+}
+
+func TestMessageRegistryNotFound(t *testing.T) {
+	r := message.MessageRegistry{}
+	_, err := r.Message("m1")
+	require.EqualError(t, err, "cannot found message with key: m1")
 }
