f17bc8de449fa1659cc12e9a75fbe4e556799553
eloylp <eloylp@protonmail.com>
2022-03-01 19:55:33 +0100

    Add wait capacity for worker pool

--- pool.go
+++ pool.go
@@ -2,23 +2,34 @@ package goomerang
 
 import (
 	"errors"
+	"sync"
 )
 
 type WorkerPool struct {
 	ch chan struct{}
+	wg *sync.WaitGroup
 }
 
 func (p *WorkerPool) Add() {
 	p.ch <- struct{}{}
+	p.wg.Add(1)
 }
 
 func (p *WorkerPool) Done() {
 	<-p.ch
+	p.wg.Done()
+}
+
+func (p *WorkerPool) Wait() {
+	p.wg.Wait()
 }
 
 func NewWorkerPool(max int) (*WorkerPool, error) {
 	if max < 1 {
 		return nil, errors.New("workerPool: min concurrency should be 1")
 	}
-	return &WorkerPool{ch: make(chan struct{}, max)}, nil
+	return &WorkerPool{
+		ch: make(chan struct{}, max),
+		wg: &sync.WaitGroup{},
+	}, nil
 }
