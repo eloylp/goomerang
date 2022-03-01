4cf028452602eb202f2432387822865f891b7932
eloylp <eloylp@protonmail.com>
2022-03-01 20:53:14 +0100

    Remove dead code

--- server/error.go
+++ server/error.go
@@ -2,26 +2,8 @@ package server
 
 import (
 	"errors"
-	"fmt"
 )
 
 var (
 	ErrClientDisconnected = errors.New("client properly closed connection")
 )
-
-type HandlerError struct {
-	Message string
-	Code    int32
-}
-
-func NewHandlerErrorWith(message string, code int32) *HandlerError {
-	return &HandlerError{Message: message, Code: code}
-}
-
-func NewHandlerError(message string) *HandlerError {
-	return &HandlerError{Message: message, Code: 0}
-}
-
-func (h HandlerError) Error() string {
-	return fmt.Sprintf("handler error code %v: %s", h.Code, h.Message)
-}
