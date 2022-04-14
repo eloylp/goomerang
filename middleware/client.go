df6532e4bedfb513fcede39f32773dd6830a51b8
eloylp <eloylp@protonmail.com>
2022-04-14 19:20:34 +0200

    Add metered client wrapper
    This will enable library users to instrument sending methods out of the middleware chain.

---
+++ middleware/client.go
@@ -0,0 +1,71 @@
+package middleware
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"google.golang.org/protobuf/proto"
+
+	"go.eloylp.dev/goomerang/client"
+	"go.eloylp.dev/goomerang/message"
+	clientMetrics "go.eloylp.dev/goomerang/metrics/client"
+)
+
+type MeteredClient struct {
+	c *client.Client
+}
+
+func NewMeteredClient(c *client.Client) *MeteredClient {
+	return &MeteredClient{c: c}
+}
+
+func (c *MeteredClient) Connect(ctx context.Context) error {
+	metricsMiddleware, err := PromHistograms(PromConfig{
+		MessageInflightTime:   clientMetrics.MessageInflightTime,
+		ReceivedMessageSize:   clientMetrics.ReceivedMessageSize,
+		MessageProcessingTime: clientMetrics.MessageProcessingTime,
+		SentMessageSize:       clientMetrics.SentMessageSize,
+	})
+	if err != nil {
+		return fmt.Errorf("goomerang: connect: instrumentation: %v", err)
+	}
+	c.RegisterMiddleware(metricsMiddleware)
+	return c.c.Connect(ctx)
+}
+
+func (c *MeteredClient) Send(msg *message.Message) (payloadSize int, err error) {
+	payloadSize, err = c.c.Send(msg)
+	if err != nil {
+		return 0, err
+	}
+	clientMetrics.SentMessageSize.Observe(float64(payloadSize))
+	return
+}
+
+func (c *MeteredClient) SendSync(ctx context.Context, msg *message.Message) (payloadSize int, response *message.Message, err error) {
+	start := time.Now()
+	payloadSize, response, err = c.c.SendSync(ctx, msg)
+	if err != nil {
+		return 0, nil, err
+	}
+	clientMetrics.SendSyncResponseTime.Observe(time.Since(start).Seconds())
+	clientMetrics.SentMessageSize.Observe(float64(payloadSize))
+	return
+}
+
+func (c *MeteredClient) Close(ctx context.Context) error {
+	return c.c.Close(ctx)
+}
+
+func (c *MeteredClient) RegisterMiddleware(m message.Middleware) {
+	c.c.RegisterMiddleware(m)
+}
+
+func (c *MeteredClient) RegisterHandler(msg proto.Message, h message.Handler) {
+	c.c.RegisterHandler(msg, h)
+}
+
+func (c *MeteredClient) RegisterMessage(msg proto.Message) {
+	c.c.RegisterMessage(msg)
+}
