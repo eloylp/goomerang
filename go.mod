231faf80cd0e4314b8dcf94496505f4ab88f8de6
eloylp <eloylp@protonmail.com>
2022-05-17 17:39:24 +0200

    Reconfiguration of test helpers
    -Add 127.0.0.1 addresses
    -Add proxy

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module go.eloylp.dev/goomerang
 go 1.18
 
 require (
+	github.com/Shopify/toxiproxy v2.1.4+incompatible
 	github.com/google/uuid v1.3.0
 	github.com/gorilla/websocket v1.4.2
 	github.com/hashicorp/go-multierror v1.1.1
@@ -18,6 +19,7 @@ require (
 	github.com/cespare/xxhash/v2 v2.1.2 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/gorilla/mux v1.7.3 // indirect
 	github.com/hashicorp/errwrap v1.0.0 // indirect
 	github.com/matttproud/golang_protobuf_extensions v1.0.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
@@ -25,5 +27,6 @@ require (
 	github.com/prometheus/common v0.32.1 // indirect
 	github.com/prometheus/procfs v0.7.3 // indirect
 	golang.org/x/sys v0.0.0-20220114195835-da31bd327af9 // indirect
+	gopkg.in/tomb.v1 v1.0.0-20141024135613-dd632973f1e7 // indirect
 	gopkg.in/yaml.v3 v3.0.0-20200313102051-9f266ea9e77c // indirect
 )
--- helper_test.go
+++ helper_test.go
@@ -3,9 +3,13 @@ package goomerang_test
 import (
 	"context"
 	"crypto/tls"
+	"net"
+	"strings"
 	"testing"
 	"time"
 
+	"github.com/Shopify/toxiproxy"
+	toxiClient "github.com/Shopify/toxiproxy/client"
 	"go.eloylp.dev/kit/pki"
 	"go.eloylp.dev/kit/test"
 
@@ -14,20 +18,62 @@ import (
 )
 
 const (
-	serverAddr = "0.0.0.0:3000"
+	proxyServerAddr   = "127.0.0.1:3000"
+	proxyAddr         = "127.0.0.1:8474"
+	serverBackendAddr = "127.0.0.1:3001"
 )
+
+var (
+	proxyServer    *toxiproxy.ApiServer
+	proxyClient    *toxiClient.Client
+	goomerangProxy *toxiClient.Proxy
+)
+
+func init() {
+	proxyServer = toxiproxy.NewServer()
+	proxyAddrParts := strings.Split(proxyAddr, ":")
+
+	go proxyServer.Listen(proxyAddrParts[0], proxyAddrParts[1])
+
+	mustWaitTCPService("localhost:8474", time.Millisecond, time.Second)
+
+	proxyClient = toxiClient.NewClient(proxyAddr)
+	var err error
+	goomerangProxy, err = proxyClient.CreateProxy("goomerang", proxyServerAddr, serverBackendAddr)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func mustWaitTCPService(addr string, interval, maxWait time.Duration) {
+	ctx, cancl := context.WithTimeout(context.Background(), maxWait)
+	defer cancl()
+	for {
+		select {
+		case <-ctx.Done():
+			panic(ctx.Err())
+		default:
+			con, conErr := net.Dial("tcp", addr)
+			if conErr == nil {
+				_ = con.Close()
+				return
+			}
+			time.Sleep(interval)
+		}
+	}
+}
 
 func PrepareServer(t *testing.T, opts ...server.Option) (s *server.Server, run func()) {
 	t.Helper()
 	is := configureServer(t, opts)
 	return is, func() {
 		go is.Run()
-		test.WaitTCPService(t, serverAddr, 50*time.Millisecond, 2*time.Second)
+		test.WaitTCPService(t, serverBackendAddr, 50*time.Millisecond, 2*time.Second)
 	}
 }
 
 func configureServer(t *testing.T, opts []server.Option) *server.Server {
-	allOpts := []server.Option{server.WithListenAddr(serverAddr)}
+	allOpts := []server.Option{server.WithListenAddr(serverBackendAddr)}
 	allOpts = append(allOpts, opts...)
 	s, err := server.NewServer(allOpts...)
 	if err != nil {
@@ -41,12 +87,12 @@ func PrepareTLSServer(t *testing.T, opts ...server.Option) (s *server.Server, ru
 	is := configureServer(t, opts)
 	return is, func() {
 		go is.Run()
-		test.WaitTLSService(t, serverAddr, 50*time.Millisecond, 2*time.Second)
+		test.WaitTLSService(t, serverBackendAddr, 50*time.Millisecond, 2*time.Second)
 	}
 }
 
 func PrepareClient(t *testing.T, opts ...client.Option) (c *client.Client, connect func()) {
-	allOpts := []client.Option{client.WithTargetServer(serverAddr)}
+	allOpts := []client.Option{client.WithTargetServer(proxyServerAddr)}
 	allOpts = append(allOpts, opts...)
 	opts = append(allOpts)
 	c, err := client.NewClient(opts...)
@@ -66,7 +112,7 @@ func SelfSignedCert(t *testing.T) tls.Certificate {
 	crt, err := pki.SelfSignedCert(pki.WithCertSerialNumber(1),
 		pki.WithCertCommonName("127.0.0.1"),
 		pki.WithCertOrganization([]string{"goomerang"}),
-		pki.WithCertIpAddresses([]string{"0.0.0.0"}),
+		pki.WithCertIpAddresses([]string{"127.0.0.1"}),
 		pki.WithCertNotBefore(time.Now()),
 		pki.WithCertNotAfter(time.Now().Add(time.Hour*24*365)),
 	)
