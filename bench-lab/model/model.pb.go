9f7b1ff6372bf46bcad86a25f1282cdfb9a1fece
eloylp <eloylp@protonmail.com>
2022-06-25 20:13:43 +0200

    bench-lab - update proto model

--- bench-lab/model/model.pb.go
+++ bench-lab/model/model.pb.go
@@ -139,6 +139,53 @@ func (x *PointReplyV1) GetStatus() string {
 	return ""
 }
 
+type BroadcastV1 struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	Message string `protobuf:"bytes,1,opt,name=message,proto3" json:"message,omitempty"`
+}
+
+func (x *BroadcastV1) Reset() {
+	*x = BroadcastV1{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_model_model_proto_msgTypes[2]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *BroadcastV1) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*BroadcastV1) ProtoMessage() {}
+
+func (x *BroadcastV1) ProtoReflect() protoreflect.Message {
+	mi := &file_model_model_proto_msgTypes[2]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use BroadcastV1.ProtoReflect.Descriptor instead.
+func (*BroadcastV1) Descriptor() ([]byte, []int) {
+	return file_model_model_proto_rawDescGZIP(), []int{2}
+}
+
+func (x *BroadcastV1) GetMessage() string {
+	if x != nil {
+		return x.Message
+	}
+	return ""
+}
+
 var File_model_model_proto protoreflect.FileDescriptor
 
 var file_model_model_proto_rawDesc = []byte{
@@ -156,10 +203,12 @@ var file_model_model_proto_rawDesc = []byte{
 	0x65, 0x76, 0x69, 0x63, 0x65, 0x44, 0x61, 0x74, 0x61, 0x22, 0x26, 0x0a, 0x0c, 0x50, 0x6f, 0x69,
 	0x6e, 0x74, 0x52, 0x65, 0x70, 0x6c, 0x79, 0x56, 0x31, 0x12, 0x16, 0x0a, 0x06, 0x73, 0x74, 0x61,
 	0x74, 0x75, 0x73, 0x18, 0x01, 0x20, 0x01, 0x28, 0x09, 0x52, 0x06, 0x73, 0x74, 0x61, 0x74, 0x75,
-	0x73, 0x42, 0x29, 0x5a, 0x27, 0x67, 0x6f, 0x2e, 0x65, 0x6c, 0x6f, 0x79, 0x6c, 0x70, 0x2e, 0x64,
-	0x65, 0x76, 0x2f, 0x67, 0x6f, 0x6f, 0x6d, 0x65, 0x72, 0x61, 0x6e, 0x67, 0x2f, 0x62, 0x65, 0x6e,
-	0x63, 0x68, 0x2d, 0x6c, 0x61, 0x62, 0x2f, 0x6d, 0x6f, 0x64, 0x65, 0x6c, 0x62, 0x06, 0x70, 0x72,
-	0x6f, 0x74, 0x6f, 0x33,
+	0x73, 0x22, 0x27, 0x0a, 0x0b, 0x42, 0x72, 0x6f, 0x61, 0x64, 0x63, 0x61, 0x73, 0x74, 0x56, 0x31,
+	0x12, 0x18, 0x0a, 0x07, 0x6d, 0x65, 0x73, 0x73, 0x61, 0x67, 0x65, 0x18, 0x01, 0x20, 0x01, 0x28,
+	0x09, 0x52, 0x07, 0x6d, 0x65, 0x73, 0x73, 0x61, 0x67, 0x65, 0x42, 0x29, 0x5a, 0x27, 0x67, 0x6f,
+	0x2e, 0x65, 0x6c, 0x6f, 0x79, 0x6c, 0x70, 0x2e, 0x64, 0x65, 0x76, 0x2f, 0x67, 0x6f, 0x6f, 0x6d,
+	0x65, 0x72, 0x61, 0x6e, 0x67, 0x2f, 0x62, 0x65, 0x6e, 0x63, 0x68, 0x2d, 0x6c, 0x61, 0x62, 0x2f,
+	0x6d, 0x6f, 0x64, 0x65, 0x6c, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
 }
 
 var (
@@ -174,14 +223,15 @@ func file_model_model_proto_rawDescGZIP() []byte {
 	return file_model_model_proto_rawDescData
 }
 
-var file_model_model_proto_msgTypes = make([]protoimpl.MessageInfo, 2)
+var file_model_model_proto_msgTypes = make([]protoimpl.MessageInfo, 3)
 var file_model_model_proto_goTypes = []interface{}{
 	(*PointV1)(nil),               // 0: company.map.PointV1
 	(*PointReplyV1)(nil),          // 1: company.map.PointReplyV1
-	(*timestamppb.Timestamp)(nil), // 2: google.protobuf.Timestamp
+	(*BroadcastV1)(nil),           // 2: company.map.BroadcastV1
+	(*timestamppb.Timestamp)(nil), // 3: google.protobuf.Timestamp
 }
 var file_model_model_proto_depIdxs = []int32{
-	2, // 0: company.map.PointV1.time:type_name -> google.protobuf.Timestamp
+	3, // 0: company.map.PointV1.time:type_name -> google.protobuf.Timestamp
 	1, // [1:1] is the sub-list for method output_type
 	1, // [1:1] is the sub-list for method input_type
 	1, // [1:1] is the sub-list for extension type_name
@@ -219,6 +269,18 @@ func file_model_model_proto_init() {
 				return nil
 			}
 		}
+		file_model_model_proto_msgTypes[2].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*BroadcastV1); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
 	}
 	type x struct{}
 	out := protoimpl.TypeBuilder{
@@ -226,7 +288,7 @@ func file_model_model_proto_init() {
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
 			RawDescriptor: file_model_model_proto_rawDesc,
 			NumEnums:      0,
-			NumMessages:   2,
+			NumMessages:   3,
 			NumExtensions: 0,
 			NumServices:   0,
 		},
